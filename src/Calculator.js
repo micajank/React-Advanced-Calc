4cc7d6148660cca2cb28b38d69bd02e71d69d7c4
micajank <micajank@umich.edu>
2020-03-10 09:09:31 -0700

    turning in homework

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,6 +2,33 @@ import React, { useState } from 'react'
 
 const Calculator = props => {
     // Declare state variables
+    const [values, setValue] = useState([]);
+
+    const handleInput = (e) => {
+        return (
+            e.target.value
+        )
+    }
+
+    const addCalc = (e, newValue) => {
+        e.preventDefault();
+        if (handleInput !== "+" && handleInput !== "-" && handleInput !== "x" &&handleInput !== "=" && handleInput !== "/") {
+            setValue([...values + newValue]);
+        } else {
+            if (handleInput === "+") {
+                setValue([...values, ...values + newValue]);
+
+            } else if (handleInput === "-") {
+
+            } else if (handleInput === "x") {
+
+            } else if (handleInput === "/") {
+                
+            } else if (handleInput === "=") {
+                
+            }
+        }
+    };
 
 
     return (
@@ -10,38 +37,40 @@ const Calculator = props => {
             <div className="calc-container">
                 <p>Values: </p>
                 <div className="answer-box">TBD</div>
-                <div className="calc-row">
+                <div className="calc-row" onClick={e => this.handleInput(e, "value")}>
                     <button className="calc-button calc-button-top">AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
                     <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-op" value={"/"}>/</button>
                 </div>
-                <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                <div className="calc-row" onClick={e => this.handleInput(e, "value")}>
+                    <button className="calc-button" value={7}>7</button>
+                    <button className="calc-button" value={8}>8</button>
+                    <button className="calc-button" value={9}>9</button>
+                    <button className="calc-button calc-button-op" value={"x"}>x</button>
                 </div>
-                <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                <div className="calc-row" onClick={e => this.handleInput(e, "value")}>
+                    <button className="calc-button" value={4}>4</button>
+                    <button className="calc-button" value={5}>5</button>
+                    <button className="calc-button" value={6}>6</button>
+                    <button className="calc-button calc-button-op" value={"-"}>-</button>
                 </div>
-                <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                <div className="calc-row" onClick={e => this.handleInput(e, "value")}>
+                    <button className="calc-button" value={1}>1</button>
+                    <button className="calc-button" value={2}>2</button>
+                    <button className="calc-button" value={3}>3</button>
+                    <button className="calc-button calc-button-op" value={"+"}>+</button>
                 </div>
-                <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                <div className="calc-row"  onClick={e => this.handleInput(e, "value")}>
+                    <button className="calc-button width-2" value={0}>0</button>
+                    <button className="calc-button" value={"."}>.</button>
+                    <button className="calc-button calc-button-op" value={"="}>=</button>
                 </div>
             </div>
         </div>
     )
+    
 }
 
-export default Calculator
+
+export default Calculator;
